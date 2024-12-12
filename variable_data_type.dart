b7aa5bf99d7f6c4fae6e4a5c0a881962c8a28696
NguyenHoangNamCT <nguyenhoangnam1710.ct@gmail.com>
2024-12-12 19:18:03 +0700

    Kiểu dữ liệu

---
+++ variable_data_type.dart
@@ -0,0 +1,13 @@
+void main(List<String> args) {
+  var nameOfVariable;
+
+  String name = 'Nam';
+
+  num age = 24;
+
+  bool hasBoy = true;
+
+  List cars = ['honda', 'toyota', 'yamaha'];
+
+  print('This is $name. he is $age');
+}
